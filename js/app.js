85181782f435f303a0b1a2ef1f3be2e77226ac7e
Jahid Mirza <Jahidmirza22@gmail.com>
2021-10-09 19:38:09 +0600

    ok

--- js/app.js
+++ js/app.js
@@ -1,18 +1,35 @@
-function () {
-    const carousel-contant = document.querySelector('.carousel-contant');
-    const leftBtn = document.querySelector('.btn-left');
-    const rightBtn = document.querySelector('.btn-right');
+const carouselContant = document.querySelector('.carousel-contant');
+const prevBtn = document.querySelector('.btn-left');
+const nextBtn = document.querySelector('.btn-right');
+const totalSlider = carouselContant.length;
+const index=0;
 
-    leftBtn.addEventListener('click', moveLeftBtn);
-    rightBtn.addEventListener('click', moveRightBtn);
+nextBtn.onclick= ()=>{
+    next('next');
+}
 
+prevBtn.onclick= ()=>{
+    next('prev');
+}
 
-    function moveLeftBtn(){
+function next(direction){
+        if (direction=="next") {
+            index++;
+            if(index==totalSlider){
+                index=0;
+            }
+        }
+        else{
+            if (index==0) {
+                index.totalSlider-1;
+            }else{
+                index--;
+            }
+        }
 
-    }
+        for ( i = 0; i < carouselContant.length; i++) {
+            carouselContant[i].classlist.remove('active');
+        }
+        carouselContant[index].classlist.add('active');
 
-    
-    function moveRightBtn(){
-
-    }
 }
