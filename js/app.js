3f35be8bc38e6d8614459ffbc82d37b70318723e
Jahid Mirza <Jahidmirza22@gmail.com>
2021-10-09 13:02:08 +0600

    ok

---
+++ js/app.js
@@ -0,0 +1,18 @@
+function () {
+    const carousel-contant = document.querySelector('.carousel-contant');
+    const leftBtn = document.querySelector('.btn-left');
+    const rightBtn = document.querySelector('.btn-right');
+
+    leftBtn.addEventListener('click', moveLeftBtn);
+    rightBtn.addEventListener('click', moveRightBtn);
+
+
+    function moveLeftBtn(){
+
+    }
+
+    
+    function moveRightBtn(){
+
+    }
+}
